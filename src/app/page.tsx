eb0760a791d1414f0979c953cf49629c074059c0
Weldon <weldonbarros179@gmail.com>
2025-07-20 13:10:00 -0300

    refactor: usar componente OverviewPage na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import OverviewPage from "@/app/overview/page"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
-      <h1 className="text-5xl font-bold">DASHBOARD</h1>
-    </main>
+    <OverviewPage/>
   );
 }
